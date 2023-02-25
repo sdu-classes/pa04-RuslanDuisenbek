e6689a99b137a1553623083572734335671ce41c
RuslanDuisenbek <124431912+RuslanDuisenbek@users.noreply.github.com>
2023-02-26 01:02:55 +0600

    Update Animal.java

---
+++ problem-B/Animal.java
@@ -0,0 +1,10 @@
+public abstract class Animal {
+    protected int legs;
+    protected Animal(int legs){
+        this.legs = legs;
+    }
+    abstract void eat();
+    public void walk(){
+        System.out.println("This animal walks "  + this.legs);
+    }
+}
